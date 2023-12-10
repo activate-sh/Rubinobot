d2cd52168880c14bede2bfe171b0140ed00f1e8c
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-10 21:25:36 +0330

    Update __init__.py

--- rubino/__init__.py
+++ rubino/__init__.py
@@ -1,7 +1,5 @@
-from rubino import Client
+from rubino.client import Client
 
 __version__ = '0.1'
 __author__ = 'amirali irvany'
 __github__ = 'https://github.com/activate-sh'
-__author_email__ = 'dev.amirali.irvany@gmail.com'
-__copyright__  = 'GNU Lesser General Public License v3 (LGPLv3)'

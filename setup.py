52f99ce6b7778b61fbf0225d4ef6f4850313157e
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-12 14:09:22 +0330

    add new files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'rubino',
+    version = '0.1',
+    author='Amirali Irvany',
+    author_email = 'dev.amirali.irvany@gmail.com',
+    description = 'none',
+    keywords = ['bot', 'Bot', 'rubino', 'rubino', 'robot'],
+    # long_description = open('README.md', encoding='utf-8').read(),
+    python_requires='~=3.6',
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/activate-sh',
+    packages = find_packages(),
+    install_requires = [],
+    classifiers = [
+    	'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ]
+)

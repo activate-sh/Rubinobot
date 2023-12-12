34e8e7dddc7ddf696b2a4a7a2a91ed6e105a576d
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-12 17:08:13 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email = 'dev.amirali.irvany@gmail.com',
     description = 'none',
     keywords = ['bot', 'Bot', 'rubino', 'rubino', 'robot'],
-    # long_description = open('README.md', encoding='utf-8').read(),
+    long_description = open('README.md', encoding='utf-8').read(),
     python_requires='~=3.6',
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/activate-sh',

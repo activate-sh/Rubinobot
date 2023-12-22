55a520ab7eef367cefadab4ef32eb628fde1bcd8
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-22 20:20:04 +0330

    Update setup.py >>> adding description and update library version

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = 'rubinobot',
-    version = '1.3',
+    version = '2.0',
     author='Amirali Irvany',
     author_email = 'dev.amirali.irvany@gmail.com',
-    description = 'none',
-    keywords = ['bot', 'Bot', 'rubino', 'rubino', 'rubinobot'],
+    description = 'Robinobot is a library for building self-robots in Robino based on API',
+    keywords = ['bot', 'Bot', 'rubino', 'rubika', 'rubinobot'],
     long_description = open('README.md', encoding='utf-8').read(),
     python_requires='~=3.6',
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/activate-sh',
-    packages = find_packages(),
-    install_requires = [],
+    install_requires = ['requests'],
     classifiers = [
     	'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
